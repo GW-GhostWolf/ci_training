8d2a6f63e5004c940ccd6a29dba33f9afc0668f8
Mike <mike@none.com>
2017-08-11 18:13:36 -0500

    intentional failing test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -14,5 +14,15 @@ namespace tests
             var generated = range.Of(() => "");
             Assert.Equal(3, generated.Count());
         }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range { Count = 3, Sort = true };
+            var values = new[] { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
+        }
     }
 }
